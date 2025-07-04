290cc756de11b5af1ba0e97d80dbd3bb103b9766
kjm10 <kjm10122@gmail.com>
2025-07-04 13:40:10 +0900

    main activity fix, home display add

--- app/src/main/java/com/example/madcamp/MainActivity.kt
+++ app/src/main/java/com/example/madcamp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,11 +13,51 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.madcamp.ui.theme.MadCampTheme
+import androidx.fragment.app.Fragment
+import com.example.madcamp.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.beginTransaction().replace(R.id.bottom_layout, HomeFragment()).commit()
+
+        navigation()
+
         enableEdgeToEdge()
+    }
+
+    private fun navigation() {
+        val bottomNavigationView = binding.bottomNavigation
+        bottomNavigationView.selectedItemId = R.id.menu_home
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.menu_calendar -> {
+                    //replaceFragment(calendarFragment())
+                    true
+                }
+                R.id.menu_people -> {
+                    //replaceFragment(peopleFragment())
+                    true
+                }
+                R.id.menu_gallery -> {
+                    //replaceFragment(galleryFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.bottom_layout, fragment).commit()
     }
 }
