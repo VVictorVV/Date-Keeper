bca7135131dc3d36487c8ab133f83d3caad035e5
kjm10 <kjm10122@gmail.com>
2025-07-05 11:19:49 +0900

    theme changed

--- app/src/main/java/com/example/madcamp/ui/theme/Color.kt
+++ app/src/main/java/com/example/madcamp/ui/theme/Color.kt
@@ -6,6 +6,14 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+val Black80 = Color(0xFF000000)
+val Grey80 = Color(0xFF808080)
+val White80 = Color(0xFFFFFFFF)
+
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Black40 = Color(0xFF000000)
+val Grey40 = Color(0xFF808080)
+val White40 = Color(0xFFFFFFFF)
--- app/src/main/java/com/example/madcamp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/madcamp/ui/theme/Theme.kt
@@ -1,6 +1,7 @@
 package com.example.madcamp.ui.theme
 
 import android.app.Activity
+import android.hardware.camera2.params.BlackLevelPattern
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,15 +13,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Black80,
+    secondary = Grey80,
+    tertiary = White80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Black40,
+    secondary = Grey40,
+    tertiary = White40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
